93feba5cf8fba7a9765bf0f7b63733d0bc6a5a22
Jisu Nam <jisunam0307@gmail.com>
2022-12-21 13:24:09 +0900

    1463 - 1로 만들기

--- src/dp/n1463/Main.java
+++ src/dp/n1463/Main.java
@@ -5,7 +5,35 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static int dp[];
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = Integer.parseInt(br.readLine());
+        dp = new int[answer+1];
+        recur(answer);
+        System.out.println(dp[answer]);
+    }
+
+    static int recur(int N) {
+        if (dp[N] == 0 && N > 1) {
+            // 6으로 나눠지는 경우
+            if (N % 6 == 0) {
+                dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+            }
+            // 3으로만 나눠지는 경우
+            else if (N % 3 == 0) {
+                dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+            }
+            // 2로만 나눠지는 경우
+            else if (N % 2 == 0) {
+                dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+            }
+            // 2와 3으로 나누어지지 않는 경우
+            else {
+                dp[N] = recur(N - 1) + 1;
+            }
+        }
+        return dp[N];
     }
 }
