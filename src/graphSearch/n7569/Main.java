1bd5caa96c112305d4e1e66ec68ae04cbc7308b3
Jisu Nam <jisunam0307@gmail.com>
2023-01-04 13:56:26 +0900

    Revert "7569 - 토마토🍅 (BFS)"
    This reverts commit 86575245ccca42a1c158fffb7ff4f4274f520614.

--- src/graphSearch/n7569/Main.java
+++ src/graphSearch/n7569/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, H;
+    static int T,N,M,H;
     static int total, current, time, answer;  // 모든 토마토 개수, 현재 익은 토마토 개수
     static int[][][] tomato;
     static boolean[][][] visited;
@@ -15,54 +15,58 @@ public class Main {
     static int[] dy = {0, 0, -1, 1, 0, 0};
     static int[] dz = {0, 0, 0, 0, -1, 1};
 
+
     public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/n7569.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        total = current = answer = time = 0;
-        tomato = new int[H][N][M];
-        visited = new boolean[H][N][M];
-        q = new LinkedList<>();
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < N; j++) {
-                st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < M; k++) {
-                    switch (tomato[i][j][k] = Integer.parseInt(st.nextToken())) {
-                        case 1:
-                            current++;
-                            q.add(new int[]{i, j, k});
-                        case 0:
-                            total++;
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+        T = Integer.parseInt(br.readLine());
+        for(int t=1;t<=T;t++) {
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
+            total = current = answer = time = 0;
+            tomato = new int[H][N][M];
+            visited = new boolean[H][N][M];
+            q = new LinkedList<>();
+            for(int i=0;i<H;i++) {
+                for(int j=0;j<N;j++) {
+                    st = new StringTokenizer(br.readLine());
+                    for(int k=0;k<M;k++) {
+                        switch (tomato[i][j][k] = Integer.parseInt(st.nextToken())) {
+                            case 1: current++; q.add(new int[]{i, j, k});
+                            case 0: total++;
+                        }
                     }
                 }
             }
+            System.out.println(total+", "+current);
+
+            if(total == current) answer = 0;
+            else answer = total == bfs() ? time : -1;
+            sb.append("#").append(t).append(" ").append(answer).append("\n");
         }
-        if (total == current) answer = 0;
-        else answer = total == bfs() ? time : -1;
-        System.out.println(answer);
+        System.out.print(sb);
         br.close();
     }
 
     static int bfs() {
-        while (!q.isEmpty() && current != total) {
+        while(!q.isEmpty() && current != total) {
             int[] curr = q.poll();
-            for (int i = 0; i < 6; i++) {
+            for(int i=0;i<6;i++) {
                 int currX = dx[i] + curr[2];
                 int currY = dy[i] + curr[1];
                 int currZ = dz[i] + curr[0];
-                if (isValid(currZ, currY, currX)) {
-                    if (tomato[currZ][currY][currX] == 0 && !visited[currZ][currY][currX]) {
-                        tomato[currZ][currY][currX] = (tomato[curr[0]][curr[1]][curr[2]] + 1);
-                        visited[currZ][currY][currX] = true;
-                        time = Math.max(time, tomato[currZ][currY][currX] - 1);
-                        current++;
-                        q.add(new int[]{currZ, currY, currX});
+                if(isValid(currZ, currY, currX)) {
+                    if(tomato[currZ][currY][currX] == 0) {
+
                     }
                 }
             }
         }
+
         return current;
     }
 
