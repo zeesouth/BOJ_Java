98fddcf373ff521e48c832b0748a4a85b2ca2d25
남지수 <anm0307@naver.com>
2023-01-06 20:33:09 +0900

    1715 - 카드 정렬하기 (PQ, Greedy)

--- src/greedy/n1715/Main.java
+++ src/greedy/n1715/Main.java
@@ -6,7 +6,6 @@ import java.util.PriorityQueue;
 
 public class Main {
     static int N, answer;
-    static int[] cal;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,14 +13,15 @@ public class Main {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for(int i=0;i<N;i++) queue.add(Integer.parseInt(br.readLine()));
-        cal = new int[N];
-        cal[0] = queue.poll();
 
-        for(int i=1;i<N;i++) {
-            cal[i] = cal[i-1]+queue.poll();
-            answer += cal[i];
+        while(N>=2 && !queue.isEmpty()) {
+            int curr1 = queue.poll();
+            int curr2 = queue.poll();
+            answer += (curr1+curr2);
+
+            if(queue.isEmpty()) break;
+            else queue.add(curr1+curr2);
         }
-
         System.out.println(answer);
         br.close();
     }
