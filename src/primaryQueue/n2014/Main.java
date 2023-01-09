bd9e139e6c44b65c7f1f5c90393dffb596fb6e66
Jisu Nam <jisunam0307@gmail.com>
2023-01-09 13:36:01 +0900

    2014 - 소수의 곱 (PQ)

--- src/primaryQueue/n2014/Main.java
+++ src/primaryQueue/n2014/Main.java
@@ -1,33 +1,40 @@
 package primaryQueue.n2014;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int K, N, count, answer;
-    static int[] arr;
+    static int K, N;
+    static long ans;
+    static long[] arr;
+    static PriorityQueue<Long> pq;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         K = Integer.parseInt(st.nextToken());
-        arr = new int[K];
+        arr = new long[K];
         N = Integer.parseInt(st.nextToken());
+        pq = new PriorityQueue<>();
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<K;i++) arr[i] = Integer.parseInt(st.nextToken());
-
-        count = 0;
-        answer = arr[0];
-
-        while(count < N) {
-            int curr = answer;
-            for(int i=0;i<arr.length;i++) {
-                while(curr%arr[i] == 0) curr /= arr[i];
-                if(curr == 1) break;
-            }
-            if(curr == 1) count++;
-            answer++;
+        for(int i=0;i<K;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            pq.add(arr[i]);
         }
-        System.out.println(answer-1);
+
+        ans = 0;
+        while(N-- > 0) {
+            ans = pq.poll();
+            for(int i=0;i<K;i++) {
+                // 정답이 2^31보다 작아야함
+                if ((ans * arr[i]) >= ((long) 2 << 30)) break;
+
+                pq.offer(ans * arr[i]);
+
+                // 현재 소수로 나누어 떨어지는 수가 발견되면 종료
+                // 중복된 값을 배제하기 위함
+                if (ans % arr[i] == 0) break;
+            }
+        }
+        System.out.println(ans);
     }
 }
