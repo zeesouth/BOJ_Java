aa0fce26ec534f78315d738c1e3181b5568dd292
Jisu Nam <jisunam0307@gmail.com>
2023-01-09 09:10:47 +0900

    2014 - 소수의 곱 (PQ) [TimeOut, 진행중...]

--- src/primaryQueue/n2014/Main.java
+++ src/primaryQueue/n2014/Main.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int K, N;
+    static int K, N, count, answer;
     static int[] arr;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,5 +15,19 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<K;i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        count = 0;
+        answer = arr[0];
+
+        while(count < N) {
+            int curr = answer;
+            for(int i=0;i<arr.length;i++) {
+                while(curr%arr[i] == 0) curr /= arr[i];
+                if(curr == 1) break;
+            }
+            if(curr == 1) count++;
+            answer++;
+        }
+        System.out.println(answer-1);
     }
 }
