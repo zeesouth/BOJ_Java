1a00efa4f015cc0ec36ecf38a5e4b0b6dc6116bb
Jisu Nam <jisunam0307@gmail.com>
2023-01-10 21:43:45 +0900

    14464 - 소가 길을 건너간 이유 4 (PQ, Greedy) [진행중.../시간초과]

--- src/primaryQueue/n14464/Main.java
+++ src/primaryQueue/n14464/Main.java
@@ -1,84 +1,86 @@
 package primaryQueue.n14464;
 // https://www.acmicpc.net/problem/14464
+// https://www.acmicpc.net/board/view/70045
 
 import java.io.*;
 import java.util.*;
 
 class Pair implements Comparable<Pair> {
     int start, end;
+
     Pair(int start, int end) {
         this.start = start;
         this.end = end;
     }
+
     @Override   // 시작 시기가 빠른 순
     public int compareTo(Pair o) {
-        if(this.start == o.start)
-            return this.end-o.end;
-        return this.start-o.start;
+        if (this.start == o.start)
+            return this.end - o.end;
+        return this.start - o.start;
     }
 }
 
 class SubPair implements Comparable<SubPair> {
     int start, end;
+
     SubPair(int start, int end) {
         this.start = start;
         this.end = end;
     }
-    @Override   // 끝날 시기가 가장 빠른 순
+
+    @Override   // 시작 시기가 빠른 순
     public int compareTo(SubPair o) {
-        if(this.end == o.end)
-            return this.start-o.start;
-        return this.end-o.end;
+        if (this.end == o.end)
+            return this.start - o.start;
+        return this.end - o.end;
     }
 }
 
 public class Main {
     static int C, N, ans;
-    static boolean[] isCheckedChicken;
-    static PriorityQueue<Integer> cow;
+    static PriorityQueue<Integer> chicken;
 
-    static PriorityQueue<Pair> chicken;
-    public static void main(String[] args) throws Exception{
+    static PriorityQueue<Pair> cow;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         C = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        cow = new PriorityQueue<>();
         chicken = new PriorityQueue<>();
-        for(int i=0;i<C;i++) cow.add(Integer.parseInt(br.readLine()));
-        isCheckedChicken = new boolean[N];
-        for(int i=0;i<N;i++) {
+        cow = new PriorityQueue<>();
+        for (int i = 0; i < C; i++) chicken.add(Integer.parseInt(br.readLine()));
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            chicken.add(new Pair(s, e));
+            cow.add(new Pair(s, e));
         }
-
         ans = 0;
-        while(!cow.isEmpty()) {
-            int currCow = cow.peek();
-            PriorityQueue<SubPair> availChicken = new PriorityQueue<>();
+        while (!cow.isEmpty()) {
+            if (chicken.isEmpty()) break;
+            int currChicken = chicken.peek();
+            PriorityQueue<SubPair> availCow = new PriorityQueue<>();
 
-            if(chicken.isEmpty()) break;
-
-            while(!chicken.isEmpty()) {
-                Pair p = chicken.peek();
-                if(p.start <= currCow && currCow <= p.end) {
-                    availChicken.add(new SubPair(p.start, p.end));
-                    chicken.poll();
-                } else break;
+            while (!cow.isEmpty()) {
+                Pair p = cow.peek();
+                if (p.start <= currChicken && currChicken <= p.end) {
+                    availCow.add(new SubPair(p.start, p.end));
+                    cow.poll();
+                } else if(p.end < currChicken) cow.poll();
+                else if(p.start > currChicken) chicken.poll();
             }
 
-            if (availChicken.isEmpty()) {
-                if(currCow < chicken.peek().start) cow.poll();
-                else if (currCow > chicken.peek().end) chicken.poll();
+            if (availCow.isEmpty()) {
+                chicken.poll();
             } else {
-                availChicken.poll();
-                cow.poll();
+                availCow.poll();
+                chicken.poll();
                 ans++;
-                while(!availChicken.isEmpty()) {
-                    SubPair a = availChicken.poll();
-                    chicken.add(new Pair(a.start, a.end));
+                while (!availCow.isEmpty()) {
+                    SubPair a = availCow.poll();
+                    cow.add(new Pair(a.start, a.end));
                 }
             }
         }
