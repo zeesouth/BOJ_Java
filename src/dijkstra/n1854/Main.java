73bbb21b8feea877058fc924eefd4a800b6e854e
Jisu Nam <jisunam0307@gmail.com>
2023-02-07 10:09:58 +0900

    1854 - K번째 최단경로 찾기 (Dijkstra)

--- src/dijkstra/n1854/Main.java
+++ src/dijkstra/n1854/Main.java
@@ -17,7 +17,7 @@ public class Main {
     static int N, M, K;
     static ArrayList<Node>[] graph;
     static boolean[] visited;
-    static int[] dist;
+    static PriorityQueue<Integer>[] dist;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,8 +31,11 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
 
         graph = new ArrayList[N+1];
-        dist = new int[N+1];
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+        dist = new PriorityQueue[N+1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+            dist[i] = new PriorityQueue<>();
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -42,46 +45,41 @@ public class Main {
             graph[a].add(new Node(b, c));
         }
 
-        for(int i=1;i<=N;i++) sb.append(dijkstra(i, K) + "\n");
+        dijkstra(1);
 
+        for(int i=1;i<=N;i++) {
+            if(dist[i].size() == K) sb.append(-dist[i].peek()).append("\n");
+            else sb.append(-1).append("\n");
+        }
 
         bw.write(sb.toString());
         bw.flush();
         br.close();
     }
 
-    static int dijkstra(int end, int k) {
+    static void dijkstra(int start) {
 
-        PriorityQueue<Integer> len = new PriorityQueue<>();
-
-        visited = new boolean[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start].add(0);
 
         PriorityQueue<Node> q = new PriorityQueue<>(
                 (o1, o2) -> o1.cost - o2.cost
         );
-        q.add(new Node(1, 0));
-        dist[1] = 0;
+        q.add(new Node(start, 0));
 
         while (!q.isEmpty()) {
             Node curr = q.poll();
 
-            if (!visited[curr.v] && curr.v != end) visited[curr.v] = true;
             for (Node next : graph[curr.v]) {
 
-                if (next.v == end) {
-                    int cost = curr.cost + next.cost;
-                    if(!len.contains(cost)) len.add(cost);
-                } else if (!visited[next.v] && dist[next.v] > curr.cost + next.cost) {
-                    dist[next.v] = curr.cost + next.cost;
-                    q.add(new Node(next.v, dist[next.v]));
+                if(dist[next.v].size() < K) {
+                    dist[next.v].add(-(curr.cost + next.cost));
+                    q.add(new Node(next.v, curr.cost + next.cost));
+                } else if (-dist[next.v].peek() > curr.cost + next.cost) {
+                    dist[next.v].poll();
+                    dist[next.v].add(-(curr.cost + next.cost));
+                    q.add(new Node(next.v, curr.cost + next.cost));
                 }
             }
         }
-
-        ArrayList<Integer> res = new ArrayList<>();
-        res.addAll(len);
-
-        return res.size() < K ? -1 : res.get(k-1);
     }
 }
