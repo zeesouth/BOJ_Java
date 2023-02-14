765907fbfc3a54ba54897c11bc8dc7222b73f901
Jisu Nam <jisunam0307@gmail.com>
2023-02-15 02:58:39 +0900

    1103 - 게임 (DP, DFS) [진행중...]

---
+++ src/dp/n1103/Main.java
@@ -0,0 +1,65 @@
+package dp.n1103;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean flag = false;
+    static int N, M, ans = -1;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int[][] map, dp;
+    static boolean[][] visited;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+        map = new int[N][M];
+        for(int i=0;i<N;i++) {
+            String s = br.readLine();
+            for(int j=0;j<M;j++) {
+                int curr = s.charAt(j);
+                if(curr == 'H') map[i][j] = -1;
+                else map[i][j] = curr-'0';
+            }
+        }
+
+        dp = new int[N][M];
+        visited = new boolean[N][M];
+
+        game(0, 0);
+        bw.write((flag ? -1 : ans)+"");
+        bw.flush();
+        br.close();
+    }
+
+    static int game(int y, int x) {
+        visited[y][x] = true;
+        dp[y][x] = 1;
+        int X = map[y][x];
+        for(int i=0;i<4;i++) {
+            int currY = y+dy[i]*X;
+            int currX = x+dx[i]*X;
+            if(isValid(currY, currX)) {
+                if (!visited[currY][currX]) dp[y][x] = Math.max(dp[y][x], game(currY, currX)+1);
+                else {
+                    if(dp[y][x] == Math.max(dp[y][x], dp[currY][currX])) flag = true;
+                    else dp[y][x] = Math.max(dp[y][x], dp[currY][currX]);
+                }
+            }
+        }
+        if(dp[y][x] != 1) ans = Math.max(ans, dp[y][x]);
+        return dp[y][x];
+    }
+
+    static int stoi(String s){
+        return Integer.parseInt(s);
+    }
+
+    static boolean isValid(int y, int x) {
+        return y >= 0 && y < N && x >= 0 && x < M && map[y][x] != -1;
+    }
+
+}
