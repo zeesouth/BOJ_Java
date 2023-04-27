ae7cdd0030ec8883571c2e3322980871d3d27a80
Jisu <jisunam0307@gmail.com>
2023-04-27 23:09:38 +0900

    17280 - 카풀 매칭 (Greedy) [왜 20점 .. ㅜ,,ㅜ]

--- src/greedy/n17280/Main.java
+++ src/greedy/n17280/Main.java
@@ -1,5 +1,7 @@
 package greedy.n17280;
 // https://www.acmicpc.net/problem/17280
+// 소가 길을 건너간 이유 4와 같은 문제
+// https://github.com/jeeneee/Algorithms/blob/master/%EB%B0%B1%EC%A4%80/%EA%B7%B8%EB%A6%AC%EB%94%94%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/17280_%EC%B9%B4%ED%92%80%20%EB%A7%A4%EC%B9%AD.cpp
 
 import java.io.*;
 import java.util.*;
@@ -16,13 +18,18 @@ public class Main {
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
 
-            PriorityQueue<Integer> customer = new PriorityQueue<>((o1,o2)->o2-o1);
+            TreeMap<Integer, Integer> customer = new TreeMap<>();
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) customer.add(Integer.parseInt(st.nextToken()));
+            for (int i = 0; i < N; i++) {
+                int val = Integer.parseInt(st.nextToken());
+                if(!customer.containsKey(val)) customer.put(val, 0);
+                customer.replace(val, customer.get(val)+1);
+            }
 
             PriorityQueue<Driver> driver = new PriorityQueue<>(
-                    (o1, o2) -> o1.end == o2.end ? o2.start - o1.start : o2.end - o1.end
+                    (o1, o2) -> o1.end == o2.end ? o1.start - o2.start : o1.end - o2.end
             );
+
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
                 int start = Integer.parseInt(st.nextToken());
@@ -31,29 +38,14 @@ public class Main {
             }
 
             int ans = 0;
-            PriorityQueue<Integer> reverseCustomer = new PriorityQueue<>((o1, o2)->o2-o1);
-            while (!driver.isEmpty()) {
-                Driver curDir = driver.peek();
-
-                if(reverseCustomer.isEmpty() && customer.isEmpty()) break;
-
-                if(!reverseCustomer.isEmpty()) {
-                    int curr = reverseCustomer.poll();
-                    if(curr >= curDir.start && curr <= curDir.end) {
-                        driver.poll();
-                        ans++;
-                    }
-                    continue;
-                }
-
-                while(!customer.isEmpty()) {
-                    int curr = customer.peek();
-                    if(curr < curDir.start) {
-                        if(reverseCustomer.isEmpty()) driver.poll();
-                        break;
-                    }
-                    else if(curr >= curDir.start && curr <= curDir.end) reverseCustomer.add(customer.poll());
-                    else customer.poll();
+            while(!driver.isEmpty()) {
+                Driver d = driver.poll();
+                Integer key = customer.ceilingKey(d.start);
+                if(key != null && key <= d.end) {
+                    ans++;
+                    int val = customer.get(key);
+                    if(val-1 == 0) customer.remove(key);
+                    else customer.replace(key, val-1);
                 }
             }
             sb.append(ans).append("\n");
@@ -63,7 +55,6 @@ public class Main {
 
     static class Driver {
         int start, end;
-
         Driver(int start, int end) {
             this.start = start;
             this.end = end;
